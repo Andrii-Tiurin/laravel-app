f8e4a2ab2c84e9142c31ed225307d3468cc8b799
Andrii Tiurin <145511448+Andrii-Tiurin@users.noreply.github.com>
2025-10-23 21:01:41 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,30 +1,17 @@
 <?php
 
-/**
- * @package     Akaunting
- * @copyright   2017-2023 Akaunting. All rights reserved.
- * @license     BSL; see LICENSE.txt
- * @link        https://akaunting.com
- */
-
 define('LARAVEL_START', microtime(true));
 
-// Check for maintenance
-if (file_exists($maintenance = __DIR__ . '/storage/framework/maintenance.php')) {
-    require $maintenance;
-}
+require __DIR__ . '/../vendor/autoload.php';
 
-// Register the auto-loader
-require __DIR__ . '/bootstrap/autoload.php';
+$app = require_once __DIR__ . '/../bootstrap/app.php';
 
-// Load the app
-$app = require_once __DIR__ . '/bootstrap/app.php';
-
-// Run the app
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
 $response = $kernel->handle(
     $request = Illuminate\Http\Request::capture()
-)->send();
+);
+
+$response->send();
 
 $kernel->terminate($request, $response);
